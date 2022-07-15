9468500a4fec6c64aca65f8ec2b37d9266029a62
AlePouroullis <alexpouroullis123@gmail.com>
2022-07-15 02:34:17 +0200

    changed file names to start lowercase

---
+++ components/layout.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from './navbar';
+import Footer from './footer';
+
+const navLinks = [
+	{title: "Home", path: "/", number:"one"},
+	{title: "Projects", path: "/projects", number:"two"},
+	{title: "Education", path: "/education", number:"three"},
+	{title: "Blog", path: "/blog", number:"four"},
+	{title: "Contact", path: "/contact", number:"five"},
+]
+
+export default function Layout({ children }){
+    /* NB: the number of elements within the main-grid-container
+    should match the number of values in grid-template-rows in the global css file*/
+  return (
+      <div className="main-grid-container">
+        <Navbar navLinks={navLinks}/>
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </div>
+  );
+}
---
+++ components/navbar.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import NavLink from './navlink';
+import Burger from './burger';
+import Link from 'next/link';
+import { useState } from 'react';
+import classNames from 'classnames';
+
+function Logo({ children }) {
+	return <div className="logo">
+		<Link href="/">
+			<a className="logo-link" href="">{children}</a>
+		</Link>
+	</div>
+}
+
+export default function Navbar({ navLinks }){
+	const [isShowNavList, setIsShowNavList] = useState(false);
+
+	const toggleIsShowNavList = () => setIsShowNavList(!isShowNavList);
+
+	const navListClassNames = classNames("nav-list");
+
+	return (
+		<nav className="navbar">
+			<Logo>Alé Pouroullis</Logo>
+			<Burger isShowNavList={isShowNavList} toggleIsShowNavList={toggleIsShowNavList}/>
+			<ul className={navListClassNames}>
+				{navLinks.map((link, index) => {
+					/* order will be used to stagger the animation
+					  of link content when in mobile view. It will be 
+						stored as an inline style variable in the <li> tags.*/
+					const order = index+1;
+
+					return (
+					<NavLink onClick={toggleIsShowNavList} key={link.title} style={{"--order": order}} exact href={link.path} className="nav-link">
+						{link.title}
+					</NavLink>)
+				}
+				)}
+			</ul>
+
+		</nav>
+	);
+}
