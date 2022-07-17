c7c60861d482071ad61aad023afe72804b8bf52c
AlePouroullis <alexpouroullis123@gmail.com>
2022-07-17 10:57:35 +0200

    started creating cards for courses on education page

--- pages/education.js
+++ pages/education.js
@@ -1,32 +1,63 @@
 import React from 'react';
 import Head from 'next/head';
-import styles from '../styles/util.module.css';
+import utilStyles from '../styles/util.module.css';
+import styles from '../styles/education.module.css';
+import courseData from '../public/courses.json' assert {type: 'json'};
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendar } from '@fortawesome/free-regular-svg-icons';
 
-function CourseCard({ courseName, courseDetails }) {
+function CourseCard({ courseName, shortDescription }) {
   return (
-    <div className="course-card">
-      <h1>{courseName}</h1>
-      <p>{courseDetails}</p>
-    </div>
+    <>
+      <dt>{courseName}</dt>
+      <dd>{shortDescription}</dd>
+    </>
   );
 }
 
 export default function EducationPage() {
+  const yearDecoder = {1: "First-year", 2: "Second-year", 3: "Third-year"}
   return (
     <>
       <Head>
         <title>Education | Alé Pouroullis</title>
       </Head>
-      <div  className={`${styles.container} ${styles['float-in']}`}>
+      <div  className={`${utilStyles.container} ${utilStyles['float-in']}`}>
         <h1>Education</h1>
         <section className="university">
-          <h2>University</h2>
-          <ul className="universities-list">
-            <li><span className="list-text">University of Cape Town</span>
-              <ul className="sub-list">
-                <li className="sub-list-item"><span className="sub-list-text"></span></li>
-              </ul>
-            </li>
+          <h2>Tertiary</h2>
+          <ul className={`${styles['main-list']}`}>
+            {courseData.degrees.map(degree => {
+            const courses = degree.courses;
+            /* sort by year */
+            courses.sort((a, b) => {
+              if (a.year < b.year){
+                return -1;
+              } else if (a.year > b.year) { 
+                return 1;
+              } else {
+                return 0;
+              }
+            })
+            return (<li className={`${styles['main-list']}`} key={degree.    universityName}>
+              <p>{degree.universityName}</p>
+              <p>{degree.type}.&nbsp;{degree.name}</p>
+              <div className="date">
+                <FontAwesomeIcon icon={faCalendar}/>
+                <date>{degree.startYear}-{degree.endYear}</date>
+              </div>
+              <dl>
+                {courses.map(course => {
+                  return (
+                    <CourseCard key={course.name} courseName={course.name}
+                      shortDescription={course.shortDescription}
+                    /> 
+                  );
+                })}
+              </dl>
+              </li>);
+            })
+            }
           </ul>
         </section>
         <section className="courses">
