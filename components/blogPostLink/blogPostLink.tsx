d907cba7451b1e210a42862a19f54f53a53afc97
AlePouroullis <alexpouroullis123@gmail.com>
2022-09-06 00:14:52 +0200

    added tags to blog post links

--- components/blogPostLink/blogPostLink.tsx
+++ components/blogPostLink/blogPostLink.tsx
@@ -1,6 +1,7 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import Link from "next/link";
 import { getHref } from "../../core/api/helpers";
+import styles from "./blogPostLink.module.css";
 
 type Props = {
   info: {
@@ -9,6 +10,7 @@ type Props = {
     description: string;
     publishedAt: Date;
     slug: string;
+    tags: { fields: { name: string } }[];
   };
 };
 
@@ -29,24 +31,37 @@ const months = {
 
 const BlogPostLink: FunctionComponent<Props> = ({ info }) => {
   const [datePublished, setDatePublished] = useState("");
-  // let date = new Date(info.publishedAt);
-  // const formattedDate = date.toLocaleString();
+
   useEffect(() => {
-   const date = new Date(info.publishedAt);
-    setDatePublished(`${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`);
+    const date = new Date(info.publishedAt);
+    setDatePublished(
+      `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   return (
     <div className="blog-post-link">
-      {/* <Link href={getHref(info.slug)} as={getNavigationLink(info.slug)}>
-       */}
-      <time className="date" dateTime={info.publishedAt.toString()}>{datePublished}</time>
+      <div className="metadata">
+        <time className="date" dateTime={info.publishedAt.toString()}>
+          {datePublished}
+        </time>
+      </div>
       <Link href={getHref(info.slug)}>
         <a className="action">
           <h3 className="heading">{info.title}</h3>
           <p className="description">{info.description}</p>
         </a>
       </Link>
+      {!!info.tags && (
+        <ul className={styles["list"]}>
+          {info.tags.map((tag, index) => (
+            <li className={styles['list-item']} key={tag.fields.name}>
+              {index !== 0 && <span className={styles.middot}>&middot;</span>}
+              <span className={styles.tag}>{tag.fields.name}</span>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
